f1d21257fdbff2974ea58e1efcf1ed5d5d99097e
Anastasia Sytsevich <drozdushka@gmail.com>
2023-03-17 17:34:22 +0300

    refactor: change footer component

--- vite-project/src/components/Footer.tsx
+++ vite-project/src/components/Footer.tsx
@@ -2,19 +2,18 @@ import React from 'react';
 import '../styles/Footer.css';
 
 class Footer extends React.Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     return (
-      <div className="footer wrapper">
-        <p className="year">©2023</p>
-        <a className="footer_link" href="https://github.com/AnastasiaSyt">
-          AnastasiaSyt
-        </a>
-        <a className="logo_rsschool" href="https://rs.school">
-          <img src="../../public/rs_school.png" alt="logo rs school" />
-        </a>
+      <div className="footer">
+        <div className="footer_wrapper wrapper">
+          <p className="year">©2023</p>
+          <a className="footer_link" href="https://github.com/AnastasiaSyt">
+            AnastasiaSyt
+          </a>
+          <a className="logo_rsschool" href="https://rs.school">
+            <img src="../../public/rs_school.png" alt="logo rs school" />
+          </a>
+        </div>
       </div>
     );
   }
