96d2226ae01bcacd55a584978389bef0b13980eb
Anastasia Sytsevich <drozdushka@gmail.com>
2023-03-17 21:33:28 +0300

    refactor: change main component

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -4,15 +4,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import About from './pages/About';
 import Main from './pages/Main';
 import Layout from './components/Layout';
-import { CardData } from './cards_data';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Main cards={CardData} />} />
-          <Route path="main" element={<Main cards={CardData} />} />
+          <Route index element={<Main />} />
+          <Route path="main" element={<Main />} />
           <Route path="about" element={<About />} />
           <Route path="*" element={<ErrorPage />} />
         </Route>
--- vite-project/src/pages/Main.tsx
+++ vite-project/src/pages/Main.tsx
@@ -2,9 +2,6 @@ import Cards from '../components/Cards';
 import React from 'react';
 
 class Main extends React.Component {
-  constructor(props) {
-    super(props);
-  }
   render() {
     return (
       <>
