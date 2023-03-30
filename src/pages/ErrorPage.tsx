06c248114406b41a27830e134cb1e6104926f772
Anastasia Sytsevich <drozdushka@gmail.com>
2023-03-30 13:12:15 +0300

    feat: made ErrorPage a functional component

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 
-class ErrorPage extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="wrapper">
-          <h1 className="main_title">404 Error Page</h1>
-        </div>
+export default function ErrorPage() {
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <h1 className="main_title">404 Error Page</h1>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default ErrorPage;
