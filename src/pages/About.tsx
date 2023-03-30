026a44b87db6f19b18b10525f42a3e3b5a2abf79
Anastasia Sytsevich <drozdushka@gmail.com>
2023-03-30 13:10:52 +0300

    feat: made about a functional component

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 
-class About extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="wrapper">
-          <h1 className="main_title">About</h1>
-          This is the first task in react course
-        </div>
+export default function About() {
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <h1 className="main_title">About</h1>
+        This is the first task in react course
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default About;
