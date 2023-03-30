e954f184600b177fe82516c52783f8b769e52b0c
Anastasia Sytsevich <drozdushka@gmail.com>
2023-03-30 13:17:59 +0300

    feat: made main a functional component

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,15 +2,11 @@ import Cards from '../components/Cards';
 import React from 'react';
 import SearchBar from '../components/SearchBar';
 
-class Main extends React.Component {
-  render() {
-    return (
-      <div className="wrapper main_wrapper">
-        <SearchBar />
-        <Cards />
-      </div>
-    );
-  }
+export default function Main() {
+  return (
+    <div className="wrapper main_wrapper">
+      <SearchBar />
+      <Cards />
+    </div>
+  );
 }
-
-export default Main;
