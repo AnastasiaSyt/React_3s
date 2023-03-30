50b2c1bc208345cd19cf9ccd824ea289a0985997
Anastasia Sytsevich <drozdushka@gmail.com>
2023-03-30 13:17:41 +0300

    feat: made footer a functional component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,17 @@
 import React from 'react';
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <div className="footer">
-        <div className="footer_wrapper wrapper">
-          <p className="year">©2023</p>
-          <a className="footer_link" href="https://github.com/AnastasiaSyt">
-            AnastasiaSyt
-          </a>
-          <a className="logo_rsschool" href="https://rs.school">
-            <img src="../../public/rs_school.png" alt="logo rs school" />
-          </a>
-        </div>
+export default function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer_wrapper wrapper">
+        <p className="year">©2023</p>
+        <a className="footer_link" href="https://github.com/AnastasiaSyt">
+          AnastasiaSyt
+        </a>
+        <a className="logo_rsschool" href="https://rs.school">
+          <img src="../../public/rs_school.png" alt="logo rs school" />
+        </a>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default Footer;
