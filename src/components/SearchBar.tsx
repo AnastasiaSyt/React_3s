b9a9d3987b656a4334c9e67c6b518a11d3351b0a
Anastasia Sytsevich <drozdushka@gmail.com>
2023-04-04 17:44:28 +0300

    fix: searchBar input

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,28 +1,29 @@
 import React, { useEffect, useState } from 'react';
 
 function SearchBar() {
-  const [searchValue, setSearchValue] = useState(localStorage.getItem('searchValue'));
+  const [searchValue, setSearchValue] = useState(localStorage.getItem('searchValue') ?? '');
 
   useEffect(() => {
-    localStorage.setItem('searchValue', String(searchValue));
+    return () => {
+      localStorage.setItem('searchValue', String(searchValue));
+    };
   }, [searchValue]);
 
   return (
     <div className="search" role="search">
       <form className="search_bar">
-        {searchValue != null && (
-          <input
-            role="search-input"
-            className="search_input"
-            type="search"
-            name="searchValue"
-            placeholder="What do you want to find?"
-            onChange={(event) => {
-              setSearchValue(event.target.value);
-            }}
-            value={searchValue}
-          />
-        )}
+        <input
+          role="search-input"
+          className="search_input"
+          type="search"
+          name="searchValue"
+          placeholder="What do you want to find?"
+          onChange={(event) => {
+            setSearchValue(event.target.value);
+          }}
+          defaultValue={searchValue}
+          value={searchValue}
+        />
       </form>
     </div>
   );
