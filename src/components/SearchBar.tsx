a38ee8f9ddccc308b4a216279d62683ef6cc9235
Anastasia Sytsevich <drozdushka@gmail.com>
2023-04-11 00:50:56 +0300

    refactor: change main page

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { type ChangeEvent } from 'react';
 
-function SearchBar() {
-  return (
-    <input
-      role="search-input"
-      className="search_input"
-      type="search"
-      name="searchValue"
-      placeholder="What do you want to find?"
-      onChange={(event) => {
-        setSearchValue(event.target.value);
-      }}
-      defaultValue={searchValue}
-      value={searchValue}
-    />
-  );
+interface SearchInputProps {
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onSubmit: (e: { preventDefault: () => void }) => void;
 }
+
+const SearchBar: React.FC<SearchInputProps> = ({ value, onChange, onSubmit }) => {
+  return (
+    <form className="search_bar" role="search" onSubmit={onSubmit}>
+      <div>
+        <input
+          value={value}
+          type="search"
+          onChange={onChange}
+          role="search-input"
+          className="search_input"
+          name="searchValue"
+          placeholder="What do you want to find?"
+        />
+      </div>
+    </form>
+  );
+};
 
 export default SearchBar;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,63 +1,97 @@
-/* eslint-disable @typescript-eslint/no-misused-promises */
+import React, { useState, useEffect } from 'react';
 import '../styles/Card.css';
-import React, { type ChangeEvent, useState } from 'react';
-import axiosInstance from '../services/api';
-import { type IImages } from '../types/IImages';
 import Cards from '../components/Cards';
+import SearchBar from '../components/SearchBar';
+import { type IImages } from '../types/IImages';
 import Preloader from '../components/Preloader';
 
-function Main() {
-  const [searchValue, setSearchValue] = useState<string>(localStorage.getItem('searchValue') ?? '');
-  const [isLoad, setLoad] = useState<boolean>(false);
-  const [cards, setCards] = useState<IImages[]>([]);
+const Main = () => {
+  const [defaultCards, setDefaultCards] = useState<IImages[]>([]);
+  const [searchCards, setSearchCards] = useState<IImages[]>([]);
+  const [showDefaultCards, setShowDefaultCards] = useState(true);
+  const [loading, setLoading] = useState(false);
 
-  const handleSubmit = async (e: ChangeEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setLoad(true);
-    try {
-      const response = await axiosInstance.get(
-        `search/photos?page=1&limit=9&query=${searchValue}&client_id=oUzXmCaN7FyevXb3pn-y_bUTpR1uBXUgPthsuwjdZjA`
+  const lsValue = localStorage.getItem('searchhvalue');
+  const [searchValue, setSearchValue] = useState(lsValue !== null ? lsValue : '');
+
+  useEffect(() => {
+    localStorage.setItem('searchhvalue', searchValue);
+    if (localStorage.getItem('searchhvalue') != null)
+      setSearchValue(JSON.parse(JSON.stringify(localStorage.getItem('searchhvalue'))));
+  }, [searchValue]);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(
+      `https://api.unsplash.com/search/photos?&per_page=9&page=1&query=random&client_id=oUzXmCaN7FyevXb3pn-y_bUTpR1uBXUgPthsuwjdZjA`
+    )
+      .then(async (res) => await res.json())
+      .then(
+        (data) => {
+          setDefaultCards(data.results);
+          setLoading(false);
+        },
+        (error) => {
+          console.error(error);
+        }
       );
-      setCards(response.data.results);
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setLoad(false);
-    }
+  }, []);
+
+  const handleSearch = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    setLoading(true);
+    const searchValue = JSON.parse(JSON.stringify(localStorage.getItem('searchhvalue')));
+    fetch(
+      `https://api.unsplash.com/search/photos?&per_page=9&page=1&query=${
+        searchValue as string
+      }&client_id=oUzXmCaN7FyevXb3pn-y_bUTpR1uBXUgPthsuwjdZjA`
+    )
+      .then(async (res) => await res.json())
+      .then(
+        (data) => {
+          setSearchCards(data.results);
+          setShowDefaultCards(false);
+          setLoading(false);
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
   };
 
-  return (
-    <div className="App">
-      {isLoad && <Preloader />}
-      {!isLoad && (
-        <div className="wrapper main_wrapper">
-          <div className="search" role="search">
-            <form className="search_bar" onSubmit={handleSubmit}>
-              {/* <SearchBar
-                searchValue={'searchValue'}
-                onChange={(event) => {
-                  setSearchValue(event.target.value);
-                }}
-              /> */}
-              <input
-                role="search-input"
-                className="search_input"
-                type="search"
-                name="searchValue"
-                placeholder="What do you want to find?"
-                onChange={(event) => {
-                  setSearchValue(event.target.value);
-                }}
-                value={searchValue}
-              />
-            </form>
-            {cards.length > 0 && <Cards cards={cards} />}
-            {cards.length === 0 && <p className="not_found">Nothing found for your request</p>}
-          </div>
+  const handlesearchValueChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+  };
+
+  if (loading) {
+    return <Preloader />;
+  } else if (!loading && !showDefaultCards) {
+    return (
+      <div className="App">
+        <div className="wrapper">
+          <SearchBar
+            value={searchValue}
+            onChange={handlesearchValueChange}
+            onSubmit={handleSearch}
+          />
         </div>
-      )}
-    </div>
-  );
-}
+        <Cards cards={searchCards} />
+      </div>
+    );
+  } else if (!loading && showDefaultCards) {
+    return (
+      <div className="App">
+        <div className="wrapper">
+          <SearchBar
+            value={searchValue}
+            onChange={handlesearchValueChange}
+            onSubmit={handleSearch}
+          />
+          <Cards cards={defaultCards} />
+        </div>
+      </div>
+    );
+  }
+};
 
 export default Main;
