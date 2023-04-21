31a5a4d414e613f167bd72566fcb7a40f2af0a4f
Anastasia Sytsevich <drozdushka@gmail.com>
2023-04-21 17:05:59 +0300

    fix: change server.ts

--- server.ts
+++ server.ts
@@ -1,54 +1,55 @@
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import express from 'express';
-import { createServer as createViteServer } from 'vite';
-import store from './src/redux/store';
+import express, { Request, Response, NextFunction } from 'express';
+import { createServer as createViteServer, ViteDevServer } from 'vite';
+
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-async function createServer() {
+async function createServer(): Promise<void> {
   const app = express();
 
-  const vite = await createViteServer({
+  const vite: ViteDevServer = await createViteServer(
+    {
     server: { middlewareMode: true },
     appType: 'custom',
-  });
+    }
+  );
 
   app.use(vite.middlewares);
+
   app.use(express.static('../src/styles'));
 
-  app.use('*', async (req, res, next) => {
+  app.use('*', async (req: Request, res: Response, next: NextFunction) => {
     const url = req.originalUrl;
-    // await store.dispatch(loadData());
 
     try {
-      let template = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf-8');
+      let template = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf-8');
       template = await vite.transformIndexHtml(url, template);
-      const { render } = await vite.ssrLoadModule('./src/entry-server.tsx');
-      const appHtml = await render(url);
-      const html = template.replace(`<!--ssr-outlet-->`, appHtml);
-      res.status(200).set({ 'Content-Type': 'text/html' }).end(html);
+      const html = template.split(`<!--ssr-outlet-->`);
 
-      const { pipe } = await render(url, {
-        // check it
+      const { render } = await vite.ssrLoadModule('/src/entry-server.tsx');
+
+      const { pipe } = render(url, {
         onShellReady() {
           res.write(html[0]);
           pipe(res);
         },
         onAllReady() {
-          res.write(html[1]);
+          res.write(html.join(''));
           res.end();
         },
       });
-    } catch (error) {
-      console.error(error);
-      vite.ssrFixStacktrace(error);
-      next(error);
+    } catch (err: unknown) {
+      vite.ssrFixStacktrace(err as Error);
+      next(err as Error);
     }
   });
 
-  app.listen(5173);
+  const port = process.env.PORT || 5173;
+
+  app.listen(port, () => console.log(`App is listening on http://localhost:${port}`));
 }
 
 createServer();
--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOMServer, {
   type PipeableStream,
-  type RenderToPipeableStreamOptions,
 } from 'react-dom/server';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import { StaticRouter } from 'react-router-dom/server';
 import AppRouter from './AppRouter';
 
-export function render(url: string, options?: RenderToPipeableStreamOptions): PipeableStream {
-  const stream = ReactDOMServer.renderToPipeableStream(
+export function render(url: string, options?: object): PipeableStream {
+  return ReactDOMServer.renderToPipeableStream(
     <Provider store={store}>
       <StaticRouter location={url}>
         <AppRouter />
@@ -17,5 +16,4 @@ export function render(url: string, options?: RenderToPipeableStreamOptions): Pi
     </Provider>,
     options
   );
-  return stream;
 }
